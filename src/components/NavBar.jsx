9fec38e70dda9e88040653b70a077f17a3da442a
sebastian705 <sebastian705@users.noreply.github.com>
2024-07-20 21:10:16 -0500

    agregue logo y cambie tamaño de botones

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useStore } from '../store/store';
+import logo from "../img/rick-and-morty.png"
 
 const NavBar = () => {
   const { setSearch, search } = useStore();
@@ -9,9 +10,9 @@ const NavBar = () => {
   }
   return (
     <header>
-      <div className='nav-title'>
-        <h2>Rick and Morty</h2>
-      </div>
+      <figure className='nav-logo'>
+        <img src={logo} alt="rick-and-morty-logo" />
+      </figure>
       <nav className='navbar'>
         <input type="search" onChange={handleSearch}
           className='input-search' placeholder='Buscar'
